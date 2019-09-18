e07c965e0fba29db1d54ccb9c721f6367a37018c
My Name <martmartez1@gmail.com>
2019-09-18 11:07:45 -0700

    Almost 100% Finsihed: Made This using html, figma, sass, javascript There is an API that give randoms words but it cost money after certain amounts of requests, maybe after I have money I will use that For now a I made a list of 149 words in a words array Added after gameover state Changed how the buttons look the words bank on the top bar refelcts which words have been used a guess counter displays how many guesses a user has left
    Still need to make the stick figure animate (maybe with html canvas), make the page look responsive, but it will be done later.

--- WebRoot/Javascript/main.js
+++ WebRoot/Javascript/main.js
@@ -1,4 +1,5 @@
 
+
 //Event listerners for buttons
 
 //Letter Button listener
@@ -9,30 +10,24 @@ document.querySelector('.word-btn').addEventListener('mouseup', WordBtn);
 //LetterBtn function
 letterArray = [];
 
-function LetterBtn()
+function LetterBtn(e)
 {
-    setTimeout(function()
+    if(e.target.className === "btn letter-btn play-again")
     {
-            document.querySelector('letter-btn').setAttribute(disabled, "true");
-    }, 1000);
+        window.location.reload();
+    }
 
     let letterGuess = document.querySelector('.letter-input').value.toLowerCase();
-
-    console.log(letterGuess);
 
     //Adds 'used' class to the letters that are used
     for(n = 1; n < 14; n++)
     {
         if(letterGuess.toUpperCase() === document.querySelector('.letA').childNodes[n].childNodes[0].data)
         {
-            console.log(document.querySelector('.letA').childNodes[n].childNodes[0].data);
-
             document.querySelector('.letA').childNodes[n].classList.add('used');
         }
         else if(letterGuess.toUpperCase() === document.querySelector('.letB').childNodes[n].childNodes[0].data)
         {
-            console.log(document.querySelector('.letB').childNodes[n].childNodes[0].data);
-
             document.querySelector('.letB').childNodes[n].classList.add('used');
         }
     }
@@ -83,6 +78,8 @@ function LetterBtn()
                                 HTMLWin.innerHTML = `<h4>You <span style="color:green">Win</span></h4>`;
                                 document.querySelector('.counter').childNodes[1].appendChild(HTMLWin);
 
+                                gameOver('letter');
+
                             }
                         }
                     }
@@ -96,9 +93,6 @@ function LetterBtn()
         console.log('Letter Not Found');
         guesses --;
 
-        //console.log('guesses left', guesses);
-        //console.log(document.querySelector('.counter').childNodes[1].innerText = `Guesses left: ${guesses}`);
-
         document.querySelector('.counter').childNodes[1].innerText = `Guesses left: ${guesses}`;
 
         //then check if letter array includes all the letters for the word in the correct gues section
@@ -111,19 +105,20 @@ function LetterBtn()
             let HTMLLose = document.createElement("div");
             HTMLLose.innerHTML = `<h4>You <span style="color:red">Lose</span>, word was: ${obj.propWord}</h4>`;
             document.querySelector('.counter').childNodes[1].appendChild(HTMLLose);
+
+            gameOver('letter');
         }
     }
+
 }
 
-function WordBtn()
+function WordBtn(e)
 {
 
-    setTimeout(function()
+    if(e.target.className === "btn word-btn play-again")
     {
-            document.querySelector('word-btn').setAttribute(disabled, "true");
-
-    }, 1000);
-
+        window.location.reload();
+    }
 
     let wordGuess = document.querySelector('.word-input').value;
     /* console.log(wordGuess); */
@@ -140,6 +135,8 @@ function WordBtn()
         let HTMLWin = document.createElement("div");
         HTMLWin.innerHTML = `<h4>You <span style="color:green">Win</span></h4>`;
         document.querySelector('.counter').childNodes[1].appendChild(HTMLWin);
+
+        gameOver('word');
 
     }
     else if(!wordGuess.toLowerCase())
@@ -159,6 +156,8 @@ function WordBtn()
             let HTMLLose = document.createElement("div");
             HTMLLose.innerHTML = `<h4>You <span style="color:red">Lose</span>, word was: ${obj.propWord}</h4>`;
             document.querySelector('.counter').childNodes[1].appendChild(HTMLLose);
+
+            gameOver('word');
         }
     }
 }
@@ -166,10 +165,167 @@ function WordBtn()
 
 //On Load
 
+//List of words
+let words = 
+[
+    "ability",
+    "able",
+    "about",
+    "above",
+    "accept",
+    "according",
+    "account",
+    "across",
+    "act",
+    "action",
+    "activity",
+    "actually",
+    "add",
+    "address",
+    "administration",
+    "admit",
+    "adult",
+    "affect",
+    "after",
+    "again",
+    "against",
+    "age",
+    "agency",
+    "agent",
+    "agree",
+    "agreement",
+    "ahead",
+    "allow",
+    "almost",
+    "alone",
+    "along",
+    "already",
+    "always",
+    "American",
+    "amount",
+    "analysis",
+    "animal",
+    "answer",
+    "anyone",
+    "anything",
+    "appear",
+    "apply",
+    "approach",
+    "area",
+    "argue",
+    "arm",
+    "around",
+    "arrive",
+    "art",
+    "article",
+    "artist",
+    "assume",
+    "attack",
+    "attention",
+    "attorney",
+    "audience",
+    "author",
+    "authority",
+    "available",
+    "avoid",
+    "away",
+    "baby",
+    "back",
+    "bag",
+    "ball",
+    "bank",
+    "bar",
+    "base",
+    "beat",
+    "beautiful",
+    "bed",
+    "behavior",
+    "behind",
+    "believe",
+    "benefit",
+    "best",
+    "better",
+    "between",
+    "business",
+    "center",
+    "central",
+    "century",
+    "certain",
+    "certainly",
+    "chair",
+    "challenge",
+    "chance",
+    "change",
+    "character",
+    "charge",
+    "check",
+    "child",
+    "choice",
+    "choose",
+    "church",
+    "citizen",
+    "city",
+    "civil",
+    "collection",
+    "college",
+    "color",
+    "come",
+    "commercial",
+    "common",
+    "community",
+    "company",
+    "compare",
+    "computer",
+    "concern",
+    "condition",
+    "conference",
+    "consider",
+    "consumer",
+    "contain",
+    "continue",
+    "control",
+    "data",
+    "daughter",
+    "day",
+    "dead",
+    "deal",
+    "death",
+    "debate",
+    "decade",
+    "decide",
+    "decision",
+    "deep",
+    "defense",
+    "degree",
+    "describe",
+    "design",
+    "despite",
+    "detail",
+    "determine",
+    "develop",
+    "development",
+    "die",
+    "difference",
+    "different",
+    "difficult",
+    "early",
+    "east",
+    "easy",
+    "eat",
+    "economic",
+    "economy",
+    "edge",
+    "education",
+    "year"
+]
+
+//Generates a rnadom number
+let randNum = Math.floor((Math.random()*words.length) + 0);
+
 //Private word
 let obj = (function()
 {
-    let val = 'Sonia';
+    let val = words[randNum];
     let _propWord = val;
 
     return{
@@ -210,15 +366,17 @@ document.querySelector('.letter-place').innerHTML= toHTML2;
 
 let guesses = 3;
 
-/* if(guesses === 0)
+
+function gameOver(kindOfButton)
 {
-    let HTMLLose = document.createElement("div");
-    HTMLLose.innerHTML = `<h2>You <span style="color:red">Lose</span>${obj.propWord}</h2>`;
-    document.querySelector('.counter').childNodes[1].appendChild(HTMLLose);
-} */
-
-console.log(document.querySelector('.counter').childNodes[1].innerText = `Guesses left: ${guesses}`);
-console.log(document.querySelector('.letter-place').childNodes[0]);
+    
+    document.querySelector(`.letter-input`).disabled = true;
+    document.querySelector('.word-input').disabled = true;
 
 
 
+    //Play Again
+    document.querySelector(`.` + kindOfButton + `-btn`).innerText = 'Play Again';
+    document.querySelector(`.` + kindOfButton + `-btn`).className +=' play-again';
+
+}
